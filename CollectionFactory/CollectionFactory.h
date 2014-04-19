04832cd7cd1a31bbfe609d859ff9de5151475680
Elliot Chance <elliotchance@gmail.com>
2014-04-19 18:39:54 +1000

    Initial Commit

---
+++ CollectionFactory/CollectionFactory.h
@@ -0,0 +1,13 @@
+//
+//  CollectionFactory.h
+//  CollectionFactory
+//
+//  Created by Elliot Chance on 19/04/2014.
+//  Copyright (c) 2014 Elliot Chance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CollectionFactory : NSObject
+
+@end
---
+++ CollectionFactory/CollectionFactory.m
@@ -0,0 +1,13 @@
+//
+//  CollectionFactory.m
+//  CollectionFactory
+//
+//  Created by Elliot Chance on 19/04/2014.
+//  Copyright (c) 2014 Elliot Chance. All rights reserved.
+//
+
+#import "CollectionFactory.h"
+
+@implementation CollectionFactory
+
+@end
