0a5cb6cea8e4e35206e28b2c21874db2f97a3fc3
Elliot Chance <elliotchance@gmail.com>
2014-04-22 00:39:13 +1000

    Refactoring

--- CollectionFactoryTests/Test.h
+++ CollectionFactoryTests/Test.h
@@ -1,11 +1,8 @@
-#ifndef Hark_Test_h
-#define Hark_Test_h
-
 #import <XCTest/XCTest.h>
 #import <OCMock/OCMock.h>
 
 // All tests should be using hamcrest with extensions :)
+#ifndef HC_SHORTHAND
 #define HC_SHORTHAND
-#import <OCHamcrestExtensions/OCHamcrest.h>
-
 #endif
+#import <OCHamcrestExtensions/OCHamcrest.h>
