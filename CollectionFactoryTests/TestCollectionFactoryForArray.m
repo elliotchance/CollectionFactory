8abb18ff56736bbed7c3945c1214b01b58fd5bcf
Elliot Chance <elliotchance@gmail.com>
2014-04-22 16:09:41 +1000

    testArrayWithJSONData

--- CollectionFactoryTests/TestCollectionFactoryForArray.m
+++ CollectionFactoryTests/TestCollectionFactoryForArray.m
@@ -32,8 +32,8 @@
 
 - (void)testArrayWithJSONData
 {
-    NSData *data = [NSData da]
-    NSArray *array = [NSArray arrayWithJsonString:@"[123,\"abc\"]"];
+    NSData *data = [@"[123,\"abc\"]" dataUsingEncoding:NSUTF8StringEncoding];
+    NSArray *array = [NSArray arrayWithJsonData:data];
     assertThat(array, contains([NSNumber numberWithInt:123], @"abc", nil));
 }
 
