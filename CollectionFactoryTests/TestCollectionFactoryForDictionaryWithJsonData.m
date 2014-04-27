aa1b30f8d841c32c776873bc890363f1cbe76353
Elliot Chance <elliotchance@gmail.com>
2014-04-27 14:22:58 +1000

    testWillReturnNilWhenJsonStringIsValidButOfDifferentJsonType

--- CollectionFactoryTests/TestCollectionFactoryForDictionaryWithJsonData.m
+++ CollectionFactoryTests/TestCollectionFactoryForDictionaryWithJsonData.m
@@ -20,7 +20,13 @@
     assertThat(obj, nilValue());
 }
 
-//- (void)testWillReturnNilWhenJsonStringIsValidButOfDifferentJsonType;
+- (void)testWillReturnNilWhenJsonStringIsValidButOfDifferentJsonType
+{
+    NSData *data = [@"[]" dataUsingEncoding:NSUTF8StringEncoding];
+    TESTING_CLASS *obj = [TESTING_CLASS TESTING_METHOD:data];
+    assertThat(obj, nilValue());
+}
+
 //- (void)testWillReturnCorrectObjectWhenJsonIsValid;
 
 @end
