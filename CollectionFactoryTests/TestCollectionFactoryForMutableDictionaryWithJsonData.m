dbe17ba1e638274ab8ab7e169314b2a9d7901978
Elliot Chance <elliotchance@gmail.com>
2014-04-27 18:16:06 +1000

    TestCollectionFactoryForMutableDictionaryWithJsonData

---
+++ CollectionFactoryTests/TestCollectionFactoryForMutableDictionaryWithJsonData.m
@@ -0,0 +1,37 @@
+#import "CollectionFactoryTestCase.h"
+
+#define TESTING_METHOD mutableDictionaryWithJsonData
+#define TESTING_CLASS NSMutableDictionary
+
+@interface TestCollectionFactoryForMutableDictionaryWithJsonData : XCTestCase <CollectionFactoryStaticTestCases>
+
+@end
+
+@implementation TestCollectionFactoryForMutableDictionaryWithJsonData
+
+- (void)testWillThrowInvalidArgumentExceptionWhenInputIsNil
+{
+    assertThat([TESTING_CLASS TESTING_METHOD:nil], willThrow(NSInvalidArgumentException));
+}
+
+- (void)testWillReturnNilWhenJsonStringIsInvalid
+{
+    TESTING_CLASS *obj = [TESTING_CLASS TESTING_METHOD:INVALID_JSON_DATA];
+    assertThat(obj, nilValue());
+}
+
+- (void)testWillReturnNilWhenJsonStringIsValidButOfDifferentJsonType
+{
+    NSData *data = [@"[]" dataUsingEncoding:NSUTF8StringEncoding];
+    TESTING_CLASS *obj = [TESTING_CLASS TESTING_METHOD:data];
+    assertThat(obj, nilValue());
+}
+
+- (void)testWillReturnCorrectObjectWhenJsonIsValid
+{
+    NSData *data = [@"{\"abc\":123}" dataUsingEncoding:NSUTF8StringEncoding];
+    TESTING_CLASS *obj = [TESTING_CLASS TESTING_METHOD:data];
+    assertThat(obj, hasEntry(@"abc", [NSNumber numberWithInt:123]));
+}
+
+@end
