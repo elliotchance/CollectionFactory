9c7aa2f2ac850b85f14c58d122a166a382412aa0
Elliot Chance <elliotchance@gmail.com>
2015-03-16 19:28:31 +1100

    testFalseWillBeConvertedToJsonString

--- CollectionFactoryTests/TestCollectionFactoryForNumber.m
+++ CollectionFactoryTests/TestCollectionFactoryForNumber.m
@@ -6,16 +6,22 @@
 
 @implementation TestCollectionFactoryForNumber
 
-- (void)testTrueWillBeConvertedToJSON
+- (void)testTrueWillBeConvertedToJsonString
 {
     NSNumber *number = [NSNumber numberWithBool:YES];
     assertThat([number jsonString], equalTo(@"true"));
 }
 
-- (void)testTrueWillBeCovertedFromJSON
+- (void)testTrueWillBeConvertedFromJsonString
 {
     NSNumber *number = [NSNumber numberWithJsonString:@"true"];
     assertThat(number, equalTo(@YES));
+}
+
+- (void)testFalseWillBeConvertedToJsonString
+{
+    NSNumber *number = [NSNumber numberWithBool:NO];
+    assertThat([number jsonString], equalTo(@"false"));
 }
 
 @end
